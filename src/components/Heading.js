bbd8173dcced22a6f9c060d0921847d026e3ee57
m-Shehzaib <98905825+muhammadshehzaib@users.noreply.github.com>
2024-04-14 02:35:28 +0500

    Working on large screens

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import "../styles/Heading.css"
+import React from "react";
+import "../styles/Heading.css";
 
-const Heading = ({text}) => {
-    return (
-        <div className="heading" >
-            <div></div>
-            <p>{text}</p>
-            <div></div>
-            
-        </div>
-    )
-}
+const Heading = ({ text }) => {
+  return (
+    <div className="heading">
+      <div></div>
+      <p>{text}</p>
 
-export default Heading
+      <div></div>
+    </div>
+  );
+};
+
+export default Heading;
